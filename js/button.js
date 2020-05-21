93fc32ae386b81e65adecd1f4ef4b4764e85eead
unknown <C:\Users\Zelich\AppData\Roaming\The Bat!>
2020-05-21 16:11:44 +0300

    Widjet

---
+++ js/button.js
@@ -0,0 +1,16 @@
+// $(".js-main-nav-open-btn").click(function() {
+/*$(".js-button").click(function() {
+    $(".js-main-nav").toggleClass("main-nav_open");
+    $(this).toggleClass("main-nav-open-btn_active");
+    $(".js-layer").toggleClass("js-layer-active");
+    $('.main-nav').toggleClass('active');
+
+    $('.main-text-dark').toggleClass('active');
+});*/
+
+$(".js-button").click(function() {
+    $('.js-dark').toggleClass('active');
+    $('.js-widjet ').toggleClass('active');
+
+
+});
